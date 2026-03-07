f10e1873f61c9e191e562aad9578d812cf09d229
YGG <referre8733@gmail.com>
2026-03-07 14:58:33 -0300

    feat(web): implement basic chat interface

---
+++ web/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>nip</title>
+		<link rel="stylesheet" href="style/css/styles.css" />
+		<script src="assets/js/chat.js"></script>
+	</head>
+	<body>
+		<main>
+			<div class="Chat">
+				<h2>nip</h2>
+				<div id="chat_window"></div>
+				<input id="message" type="text" placeholder="nip está à sua disposição! ...">
+				<button onclick="sendMessage()">Enviar</button>
+			</div>
+		</main>	
+	</body>
+</html>
